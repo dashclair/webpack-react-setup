7453b076364f843774de6da63a06e7fe08aa7a37
Dasha <darya.gapeeva.01@gmail.com>
2024-02-13 16:25:24 +0300

    feat: added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import './styles.css';
+import cat from './img/crying-cat.gif';
+import heart from './img/heart-svgrepo-com.svg'
+import { useState } from 'react';
+
+
 export const App = () => {
+    const [state, setState] = useState(0)
+    
+    const handleIncrement = () => {
+        setState(state+1)
+    }
+    
     return (
-        <div>
-            Template
+        <div className = 'main'>
+            <button onClick={handleIncrement} className='button'>
+            share heart
+            </button>
+            <p>{state}</p>
+            <img className='heart' src={heart}/>
+            <img src={cat}/>
         </div>
     )
 }
